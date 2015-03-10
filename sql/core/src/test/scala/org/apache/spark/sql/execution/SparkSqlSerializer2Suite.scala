82919d5bc9b07a5b39a4f9b10a77420b835047f0
Yin Huai <yhuai@databricks.com>
2015-03-10 10:55:04 -0700

    wip

--- sql/core/src/test/scala/org/apache/spark/sql/execution/SparkSqlSerializer2Suite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/SparkSqlSerializer2Suite.scala
@@ -41,5 +41,59 @@ class SparkSqlSerializer2Suite extends QueryTest {
       df.groupBy("stringCol").sum(),
       (1 to 10).map(i => Row(i.toString, i, i.toDouble, i.toLong))
     )
+
+    sql("SET spark.sql.useSerializer2=false")
   }
+
+  /*
+  test("simple serializer2 v2") {
+    val df =
+      jsonRDD(sparkContext.parallelize((1 to 10).map(i =>
+        s"""
+           |{"intCol":${i},
+           |"doubleCol":${i.toDouble},
+           |"longCol":${i.toLong},
+           |"stringCol1":"${i.toString}",
+           |"stringCol2":"${i.toString}"}""".stripMargin)))
+
+    df.registerTempTable("serializer2v2")
+
+    sql("SET spark.sql.useSerializer2=true")
+    sql("SET spark.sql.useSerializer2.version=2")
+
+    sql("SELECT stringCol1, sum(doubleCol) FROM serializer2v2 GROUP BY stringCol1").explain()
+
+    checkAnswer(
+      sql("SELECT stringCol1, sum(doubleCol) FROM serializer2v2 GROUP BY stringCol1"),
+      (1 to 10).map(i => Row(i.toString, i.toDouble))
+    )
+
+    sql(
+      """
+        |SELECT x.stringCol2, avg(y.intCol), sum(x.doubleCol)
+        |FROM serializer2v2 x JOIN serializer2v2 y ON (x.stringCol1 = y.stringCol1)
+        |GROUP BY x.stringCol2
+      """.stripMargin).explain()
+
+    checkAnswer(
+      sql(
+        """
+          |SELECT x.stringCol2, avg(y.intCol), sum(x.doubleCol)
+          |FROM serializer2v2 x JOIN serializer2v2 y ON (x.stringCol1 = y.stringCol1)
+          |GROUP BY x.stringCol2
+        """.stripMargin),
+      (1 to 10).map(i => Row(i.toString, i, i.toDouble, i.toLong))
+    )
+
+    df.orderBy("stringCol", "doubleCol", "doubleCol").explain()
+
+    checkAnswer(
+      df.orderBy("stringCol", "doubleCol", "doubleCol"),
+      df.collect().toSeq
+    )
+
+    sql("SET spark.sql.useSerializer2=false")
+    sql("SET spark.sql.useSerializer2.version=1")
+  }
+  */
 }
